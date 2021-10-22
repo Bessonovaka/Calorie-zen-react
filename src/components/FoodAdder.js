4f721ed60317f93642706b9ca4b12bcb07f42330
bessonovaka <bessonova.en@mail.ru>
2021-10-22 15:26:53 +0300

    feat: добавить форму

--- src/components/FoodAdder.js
+++ src/components/FoodAdder.js
@@ -5,18 +5,33 @@ class FoodAdder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      foodList: []
+      food: '',
+      calories: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = () => {
+  handleChange = (e) => {
+    this.setState({
+   [e.target.name]: e.target.value
+ })
+  }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({'':''})
+    
+    this.props.handleSubmit(this.state.food, this.state.calories);
   }
 
   render() {
+    // добавьте элементами обработчики handleSubmit и handleChange
     return (
-      <form className="food-adder">
-        <input type="text" placeholder="Добавьте продукты" />
+      <form className="food-adder" onSubmit={this.handleSubmit}>
+        <input name="food" onChange={this.handleChange} value={this.state.food} type="text" required placeholder="Добавьте продукты" />
+        <input name="calories" onChange={this.handleChange} value={this.state.calories} type="number" required placeholder="Калории" />
         <button className="food-adder__submit"></button>
       </form>
     );
